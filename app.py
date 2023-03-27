8a36c08ae67bbde51cbc53411ee10267c8f42bdf
andyhoang <andy@parcelperform.com>
2023-03-27 15:19:54 +0700

    use testclient for test

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import os
+import logging
+
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
-import os
-import logging
 
 from gitlab.payload.issue_event import IssueEventPayload
 
@@ -12,6 +13,7 @@ app = FastAPI()
 SLACK_CHANNEL = os.getenv("SLACK_CHANNEL")
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 
+slack_client = WebClient(token=SLACK_BOT_TOKEN)
 
 @app.post("/gitlab-webhook")
 async def handler(request: Request, response: Response):
@@ -25,19 +27,18 @@ async def handler(request: Request, response: Response):
     return JSONResponse(content={"success": False})
 
 async def send_to_slack(message: str):
-  client = WebClient(token=SLACK_BOT_TOKEN)
-
   try:
-    response = client.chat_postMessage(
+    response = slack_client.chat_postMessage(
       channel=SLACK_CHANNEL,
       text=message
     )
     return response
 
   except SlackApiError as e:
-    print(e)
-    return None
+    logging.error(e)
+    raise e
 
 if __name__ == "__main__":
   import uvicorn
-  uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+  uvicorn.run("app:app", host="0.0.0.0",
+              reload=True, port=int(os.environ.get("PORT", 8080)))
--- tests/integration/test_call_api.py
+++ tests/integration/test_call_api.py
@@ -1,18 +1,26 @@
-import requests
+from fastapi.testclient import TestClient
 import json
 import os
 
-url = "https://0265-2001-ee0-500f-2010-c8b4-7b15-e4e8-ef21.ap.ngrok.io/gitlab-webhook"
-headers = {
-    "Content-Type": "application/json",
-    "X-Gitlab-Event": "Push Hook"
-}
-
+# Import FastAPI app to test
+from app import app as _app
 
 def test_call_api():
+    # Load data from file
     json_file_path = os.path.join(os.path.dirname(__file__),"data.json")
     with open(json_file_path, "r") as file:
         data = json.load(file)
-    response = requests.post(url, headers=headers, json=data)
-    assert response.status_code == 200
+    # Create a TestClient instance
+        client = TestClient(_app)
 
+        # Make the request and get the response
+        response = client.post(
+            "/gitlab-webhook",
+            headers={
+                "Content-Type": "application/json",
+                "X-Gitlab-Event": "Push Hook",
+            },
+            json=data,
+        )
+        __import__('pdb').set_trace()
+        assert response.status_code == 200
